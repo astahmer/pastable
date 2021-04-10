fb6760a2ce17c49c41e098f76a8734f04fe51548
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-04-10 21:19:51 +0200

    feat(typings): add more  to common/react

--- packages/typings/src/react.ts
+++ packages/typings/src/react.ts
@@ -1,9 +1,19 @@
-import { Dispatch, ReactNode, SetStateAction } from "react";
+import { Dispatch, MutableRefObject, ReactNode, SetStateAction } from "react";
+
+
 import { AnyFunction } from "./common";
 
 export interface WithChildren {
     children: ReactNode;
 }
+
+export interface WithRef {
+    ref?: MutableRefObject<any>;
+}
+export interface WithClassName {
+    className?: string;
+}
+
 export type SetState<T = any> = Dispatch<SetStateAction<T>>;
 
 export type OnSubmit<Values = any, Return = void> = (values: Values) => Return;
