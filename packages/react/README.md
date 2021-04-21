fc39f1ca3a57f99bc56490a5a94b4e382b66be3f
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-04-21 23:20:22 +0200

    feat(react): init pkg

---
+++ packages/react/README.md
@@ -0,0 +1,9 @@
+# @pastable/react
+
+Re-exports :
+
+-   @pastable/use-event
+-   @pastable/use-force-update
+-   @pastable/use-is-mounted
+-   @pastable/use-query-params
+-   and react typings from @pastable/typings
---
+++ packages/react/src/index.ts
@@ -0,0 +1,5 @@
+export * from "@pastable/use-event";
+export * from "@pastable/use-force-update";
+export * from "@pastable/use-is-mounted";
+export * from "@pastable/use-query-params";
+export { WithChildren, WithRef, WithClassName, WithOnClick, WithOnSubmit, OnSubmit, SetState } from "@pastable/typings";
