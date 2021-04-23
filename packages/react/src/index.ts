7ccbdae7f51726a211efc7692a78ffe6d8178721
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-04-23 23:51:27 +0200

    feat(react): add useSelection/useUpdateEffect

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -2,4 +2,6 @@ export * from "@pastable/use-event";
 export * from "@pastable/use-force-update";
 export * from "@pastable/use-is-mounted";
 export * from "@pastable/use-query-params";
+export * from "@pastable/use-selection";
+export * from "@pastable/use-update-effect";
 export { WithChildren, WithRef, WithClassName, WithOnClick, WithOnSubmit, OnSubmit, SetState } from "@pastable/typings";
