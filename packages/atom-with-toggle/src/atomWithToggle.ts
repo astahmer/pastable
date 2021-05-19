39ec9b8f1b1ea56429173dd754b39eeb9f400d11
Alexandre Stahmer <alexandre.stahmer@gmail.com>
2021-05-19 10:25:24 +0200

    types(atomWithToggle): mv to fn return type

--- packages/atom-with-toggle/src/atomWithToggle.ts
+++ packages/atom-with-toggle/src/atomWithToggle.ts
@@ -1,12 +1,16 @@
 import { WritableAtom, atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 
-export function atomWithToggle(initialValue?: boolean, key?: string, storage?: any) {
+export function atomWithToggle(
+    initialValue?: boolean,
+    key?: string,
+    storage?: any
+): WritableAtom<boolean, boolean | undefined> {
     const storeAtom = key ? atomWithStorage(key, initialValue!, storage) : null;
     const anAtom: any = atom(initialValue, (get, set, state?: boolean) => {
         const update = state ?? !get(anAtom);
         set(anAtom, update);
         storeAtom && set(storeAtom, update);
     });
-    return anAtom as WritableAtom<boolean, boolean | undefined>;
+    return anAtom;
 }
